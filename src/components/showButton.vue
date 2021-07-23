7f3e5af5d376221634b59159ac01dbfb08f298bb
will.wei <650893933@qq.com>
2021-07-23 22:04:22 +0800

    src

---
+++ src/components/showButton.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="show-button">
+        
+        <p v-if="!isShowCalendar" @click="showNotice">{{ text }}</p>  
+        <p v-if="isShowCalendar" @click="showCalendar">{{ text }}</p>   
+    </div> 
+</template>
+
+<script>
+export default {
+  methods: {
+    showCalendar () {
+      let url = '/pages/calendar/main'
+      wx.navigateTo({ url })
+    },
+    showNotice () {
+      let url = '/pages/notice/main'
+      wx.navigateTo({ url })
+    }
+  },
+  computed: {
+    isShowCalendar () {
+      return this.isCalendar
+    }
+  },
+  props: {
+    text: String,
+    isCalendar: Boolean
+  },
+  data: {
+  }
+}
+</script>
+
+<style>
+.show-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 2.5rem;      
+  height: 1rem;      
+  background-color:#ffffff;
+  opacity:0.8;
+  border: 1px solid rgba(238, 63, 77, 1);
+  border-radius: 30%;      
+  -moz-border-radius: 30%;      
+  -webkit-border-radius: 30%; 
+}
+.show-button>p {
+    color: rgba(238, 63, 77, 1);
+    font-size: 0.36rem;
+}
+
+</style>
---
+++ src/pages/notice/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="notice">
+      <div class="title">
+          为什么要数胎动？
+      </div>
+      <div class="content">
+          胎动反映了胎儿在妈妈子宫内的安危状况。如果胎动出现异常，则可能是胎儿出现宫内缺氧。依靠妈妈的自我监控，每天掌握胎动变化的情况，可以随时了解宝宝在子宫内的状态，及早发现问题。
+          通常从孕28周开始，产检医生都会要求准妈妈每天数胎动，准妈妈可以以此检测胎宝宝的健康，防止缺氧，找到宝贝的行为规律，培养好性情，与宝宝亲密互动，建立亲子感情
+      </div>
+
+      <div class="title">
+          怎样数胎动?
+      </div>
+      <div class="content">
+          每日三次计数法
+          在每天早、中、晚固定时间各数一小时，每小时胎动大于3次，则表明胎儿情况良好。5分钟内连续的胎动算做一次，胎动过多或过少都要引起孕妈妈的注意，要及时去医院检查胎儿是否缺氧。
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+.notice {
+    padding-left: 0.3rem;
+    padding-right: 0.3rem;
+}
+.title {
+    padding-top: 0.4rem;
+    display: flex;
+    justify-content: flex-start;
+    font-size: 0.38rem;
+    font-family: '华文细黑';
+    font-weight: bold;
+    color: rgba(238, 63, 77, 1);
+}
+.content {
+    padding-top: 0.3rem;
+    display: flex;
+    justify-content: flex-start;
+    font-size: 0.35rem;
+    font-family: '华文细黑';
+}
+</style>
